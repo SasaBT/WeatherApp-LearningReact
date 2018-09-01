9d611b9041fb02c2c2b8343ef9b8b09035d8a3f6
Sasa <sasa.basara@gmail.com>
2018-09-01 19:42:22 -0400

    First half of the video

---
+++ src/components/Titles.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Titles = () => (
+
+    <div>
+    
+        <h1>Weather Finder</h1>
+        <p>Find out weather and more!</p>
+
+    </div>
+
+)
+
+export default Titles;
